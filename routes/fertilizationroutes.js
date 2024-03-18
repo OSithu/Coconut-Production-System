a859d89184c14ce9c3926011bc5a71e57024471d
Nimsi Dharmaratne <nimsidinara@gmail.com>
2024-03-18 17:59:45 +0530

    add create and update function to fertilization model

--- routes/fertilizationroutes.js
+++ routes/fertilizationroutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const fertilization =require('../models/fertilizationModel');
 
+
 //send requests 
 const router = express.Router();
 
 //save Fertilization records
 
-router.post('/post/save',async (req,res)=>{
+router.post('/fertilizationrec/save',async (req,res)=>{
     //instantiation 
     try{
     let newFertilizationRecord=new fertilization(req.body);
@@ -25,4 +26,19 @@ router.post('/post/save',async (req,res)=>{
        
         }
     });  
+
+ //View Fertilization Records
+router.get('/fertilizationrec', async (req, res) => {
+    try {
+        const fertilizationrecords = await fertilization.find().exec();
+        return res.status(200).json({
+            success: true,
+            existingRecords: fertilizationrecords
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
 module.exports =router;
