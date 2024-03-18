7d6a4b1325a48c9e070a5fd3b867ee27e632688d
Heshara1127 <IT22207586@my.sliit.lk>
2024-03-18 17:53:55 +0530

    editFiles

--- routes/qualitycontrolRoutes.js
+++ routes/qualitycontrolRoutes.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const qcmposts = require('../models/qcmposts');
 
 const router = express.Router();
 
 //save posts
-router.post('/qualityrecords/save',(req,res)=>{
-
+router.post("/qualityrecords/save", async (req, res) => {
+  try {
     let newRecord = new qcmposts(req,body);
 
-    newRecord.save((err) =>{
-        if(err){
-            return res.status(400 ).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:"Saved successfully"
-        });
-    });
+    await newRecord.save();
 
+    return res.status(200).json({
+      success: "Details saved successfully",
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
