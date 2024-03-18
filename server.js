5f9eea4a9c5786d992d8c815b40192237ee9b9f9
IT22358684 <it22358684@my.sliit.lk>
2024-03-18 11:29:02 +0530

    Database connection
    creating server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+//import express
+const express = require('express');
+
+//import mongoose
+const mongoose = require('mongoose');
+
+const app = express();
+
+//declaring server running port
+const port = 8000;
+app.listen(port, () =>{
+    console.log(`App is running on ${port}`);
+});
+
+//creating DB connection
+const db_url = 'mongodb+srv://itpproject:bvS6Hoo0uVhIKL3v@atlascluster.ktimcix.mongodb.net/';
+mongoose.connect(db_url)
+.then(() =>{
+    console.log('DB connected');
+})
+.catch((err) => console.log('DB connection error',err));
+
+
