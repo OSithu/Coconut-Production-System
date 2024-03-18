4079668938bb07f43ef6d9e3237373bcebb8f661
IT22916280 <127727986+IT22916280@users.noreply.github.com>
2024-03-18 19:29:11 +0530

    edit server.js

--- routes/employeedetails.js
+++ routes/employeedetails.js
@@ -1,23 +1,47 @@
 const express =  require('express');
-const employeeDetails = require('../models/employeedetails')
+const employeeDetails = require('../models/employeedetails');
+const employeedetails = require('../models/employeedetails');
 
 const router = express.Router();
 
 //save post
-router.post('/employee/save',(req,res)=>{
+router.post('/employee/save',async(req,res)=>{
+    try{
 
     let newEmployee = new employeeDetails(req.body);
 
-    newEmployee.save((err) =>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:"employee added successfully"
-        });
-    });
+   //save records
+   await newEmployee.save();
+
+   // return success responde
+   return res.status(200).json({
+    success:"employee added successfully"
 });
+    }catch (err){
+        return res.status(400).json({
+            error:err.message
+        });
+        
+    }
+});
+
+
+//View Spread Records
+router.get('employee/view', async (req, res) => {
+    try {
+        const records = await employeedetails.find().exec();
+        return res.status(200).json({
+            success: true,
+            existingRecords: records
+        });
+    } catch (err) {
+        return res.status(400).json({
+            error: err.message
+        });
+    }
+});
+
+
+     
 
 module.exports = router;
