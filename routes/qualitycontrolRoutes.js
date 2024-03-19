459da181580ee4da0d4433c35ddbf2b46a6e8c45
Heshara1127 <IT22207586@my.sliit.lk>
2024-03-19 18:21:18 +0530

    editRouteFile

--- routes/qualitycontrolRoutes.js
+++ routes/qualitycontrolRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //save posts
 router.post("/qualityrecords/save", async (req, res) => {
   try {
-    let newRecord = new qcmposts(req,body);
+    let newRecord = new qcmposts(req.body);
 
     await newRecord.save();
 
@@ -15,7 +15,55 @@ router.post("/qualityrecords/save", async (req, res) => {
     });
   } catch (err) {
     return res.status(400).json({
-      error: err,
+      error: err.message
+    });
+  }
+});
+
+router.get("/records", async (req, res) => {
+  try {
+    const qualityrecords = await qcmposts.find().exec();
+
+    return res.status(200).json({
+      success: true,
+      existingPosts: qualityrecords
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+//update posts
+router.put("/qualityrecords/update/:id",async (req, res) => {
+  try{
+    await qcmposts.findByIdAndUpdate(req.params.id,{ $set: req.body}).exec();
+
+    return res.status(200).json({
+      success: "Updated Successfully",
+    });
+  }catch(err){
+        return res.status(400).json({
+           error: err.message,
+        });
+  }
+});
+
+//delete posts
+
+router.delete("/qualityrecords/delete/:id",async (req, res) => {
+  try{
+    const deletedRecord = await qcmposts.findByIdAndRemove(req.params.id).exec();
+
+    return res.json({
+      message:"Delete Successfull",
+      deletePost
+    });
+  }catch(err){
+    if(err) return res.status(400).json({
+      message:"Delete unsuccessfully",
+      error:err.message,
     });
   }
 });
