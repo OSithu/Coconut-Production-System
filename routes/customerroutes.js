acc34d12f7d4998058aab302cf23f3ae8ab9ed54
IT22886804 <chethanadassanayake747@gmail.com>
2024-03-19 10:04:31 +0530

    edit customer route

--- routes/customerroutes.js
+++ routes/customerroutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const CustomerDetails = require("../models/customerposts");
+const customerposts = require("../models/customerposts");
 
 const router = express.Router();
 
@@ -19,5 +20,108 @@ router.post("/cusDetails/save", async (req, res) => {
     });
   }
 });
+
+//get posts
+
+// router.get('/cusDetails',(req,res)=>{
+//   CustomerDetails.find().exec((err,cusDetails) =>{
+//     if(err){
+//       return res.status(400).json({
+//         error:err
+//       });
+//     }
+//     return res.status(200).json({
+//       success: true,
+//       existingRecords:cusDetails,
+//     });
+//   });
+// });
+
+//get posts
+router.get("/cusDetails", async (req, res) => {
+  try {
+    const cusDetails = await CustomerDetails.find().exec();
+    return res.status(200).json({
+      success: true,
+      existingRecords: cusDetails,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+});
+
+//update posts
+// router.put('/cusDetails/update/:id',(req,res)=>{
+//   CustomerDetails.findByIdAndUpdate(
+//     req.params.id,
+//     {
+//       $set: req.body,
+//     },
+//     (err, cusDetails) => {
+//       if(err) {
+//         return res.status(400).json({ error: err });
+//       }
+
+//       return res.status(200).json({
+//         success: "Updated successfully",
+//       });
+//     }
+//   );
+// });
+
+//update posts
+router.put("/cusDetails/update/:id", async (req, res) => {
+  try {
+    await CustomerDetails.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    }).exec();
+
+    return res.status(200).json({
+      success: "Updated Successfully",
+    });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+});
+
+//delete post
+// router.delete("/cusDetails/delete/:id", (req, res) => {
+//   CustomerDetails.findByIdAndDelete(req.params.id).exec(
+//     (err, deletedRecords) => {
+//       if (err)
+//         return res.status(400).json({
+//           message: "Delete unsuccessful",
+//           err,
+//         });
+//       return res.json({
+//         message: "Delete Successfull",
+//         deletedRecords,
+//       });
+//     }
+//   );
+// });
+
+
+//delete post
+router.delete("/cusDetails/delete/:id", async (req, res) => {
+  try {
+    const deletedRecords = await CustomerDetails.findByIdAndDelete(req.params.id).exec();
+
+    return res.json({
+      message: "Delete Successfully",
+      deletedRecords,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: "Deleted unsuccessfully",
+      error: err.message,
+    });
+    }
+  });
+
 
 module.exports = router;
