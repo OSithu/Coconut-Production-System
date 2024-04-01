faf41cb96e58353ca0392abb166b12c4371c398d
Heshara1127 <IT22207586@my.sliit.lk>
2024-04-02 00:36:50 +0530

    editqualitycontrolRoutes.jsFile

--- routes/qualitycontrolRoutes.js
+++ routes/qualitycontrolRoutes.js
@@ -3,7 +3,7 @@ const qcmposts = require('../models/qcmposts');
 
 const router = express.Router();
 
-//save posts
+//save records
 router.post("/qualityrecords/save", async (req, res) => {
   try {
     let newRecord = new qcmposts(req.body);
@@ -20,7 +20,7 @@ router.post("/qualityrecords/save", async (req, res) => {
   }
 });
 
-//get posts
+//get record
 router.get("/qrecords", async (req, res) => {
   try {
     const qualityrecords = await qcmposts.find().exec();
@@ -36,7 +36,7 @@ router.get("/qrecords", async (req, res) => {
   }
 });
 
-//get a specific post
+//get a specific record
 
 router.get("/qrecords/:id",(req,res)=> {
 
@@ -56,7 +56,7 @@ router.get("/qrecords/:id",(req,res)=> {
   
   
 
-//update posts
+//update record
 router.put("/qualityrecords/update/:id",async (req, res) => {
   try{
     await qcmposts.findByIdAndUpdate(req.params.id,{ $set: req.body}).exec();
@@ -71,21 +71,35 @@ router.put("/qualityrecords/update/:id",async (req, res) => {
   }
 });
 
-//delete posts
+//delete record
 
-router.delete("/qualityrecords/delete/:id",async (req, res) => {
-  try{
-    const deletedRecord = await qcmposts.findByIdAndRemove(req.params.id).exec();
+// router.delete("/qualityrecords/delete/:id",async (req, res) => {
+//   try{
+//     const deletedRecord = await qcmposts.findByIdAndRemove(req.params.id).exec();
 
-    return res.json({
-      message:"Delete Successfull",
-      deletedRecord
-    });
-  }catch(err){
-    if(err) return res.status(400).json({
-      message:"Delete unsuccessfull",
-      error:err.message,
-    });
+//     return res.json({
+//       message:"Delete Successfull",
+//       deletedRecord
+//     });
+//   }catch(err){
+//     if(err) return res.status(400).json({
+//       message:"Delete unsuccessfull",
+//       error:err.message,
+//     });
+//   }
+// });
+
+router.delete("/qualityrecords/delete/:id", async (req, res) => {
+  try {
+      const deleteRecords = await qcmposts.findByIdAndDelete(req.params.id).exec();
+      return res.status(200).json({
+          message: "Records Deleted Successfully",
+          deleteRecords,
+      });
+  } catch (err) {
+      return res.status(400).json({
+          error: err.message
+      });
   }
 });
 
