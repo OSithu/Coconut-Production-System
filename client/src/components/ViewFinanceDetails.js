8a9fc6a88374033f20ad06d008c1bfec18effb2e
Dishani Kulasekara <149486947+dish1307@users.noreply.github.com>
2024-04-07 19:04:32 +0530

    edit files

--- client/src/components/ViewFinanceDetails.js
+++ client/src/components/ViewFinanceDetails.js
@@ -1,80 +1,93 @@
-import React, { Component } from 'react';
+import React,{ useEffect,useState } from "react";
 import axios from 'axios';
+import {Link} from 'react-router-dom'
 
-export default class ViewFinanceDetails extends Component {
-  constructor(props) {
-    super(props);
+const ViewFianceDetails = () => {
 
-    this.state = {
-      financeRoute: []
-    };
-  }
+  const [allDetails, setAllDetails] = useState([]);
 
-  componentDidMount() {
-    this.retrieveFinanceRecords();
-  }
+  useEffect(() => {
 
-  retrieveFinanceRecords() {
-    axios.get("http://localhost:8000/financeRecords")
+    const getDetails = async () =>{
+
+      await axios.get(`http://localhost:8000/financeRecords`)
       .then((res) => {
-        if (res.data.success) {
-          this.setState({
-            financeRoute: res.data.existingfinance
-          });
-          console.log(this.state.financeRoute);
+        setAllDetails(res.data.existingfinance);
+        console.log('Status:' +res.data.success);
+      })
+      .catch((err) =>{
+        if(err.reponese){
+          console.log(err.response.data.error)
         }
       })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    }
+
+    getDetails();
+},[])
+
+const handleDelete = async (id) =>{
+
+  try{
+    const confirm = window.confirm('Are you sure you want to delete?');
+
+    if(confirm){
+      await axios.delete(`http://localhost:3000/financeRecords/delete/${id}`)
+      .then((res) =>{
+        alert(res.data.message);
+        console.log(res.data.message);
+        setAllDetails(allDetails.filter(finance => finance._id != id));
+      })
+      .catch((err) =>{
+        if(err.responce){
+          console.log(err.responce.data.message);
+        }else{
+          console.log("Error occur while processing your axios delete");
+        }
+      })
+    }else{
+      alert('Deletion Cancel');
+    }
+    }
+    catch(err){
+      console.log('HandleDelete function failed! error'+ err.message);
+    }
   }
 
-  onDelete = (id) =>{
-    axios.delete(`http://localhost:8000/financerecords/delete/${id}`).then(res =>{
-        alert("Successfully deleted");
-        this.retrieveFinanceRecords();
-    })
-  }
 
-  render() {
-    return (
-      <div>
-       <p>All Financial Transactions</p>
-       <table className="table">
-        <thead>
+return (
+  <div>
+        <p>All Financial Transactions</p>
+        <table className="table">
+         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Date</th>
-            <th scope="col">Type</th>
+             {/* <th scope="col">#</th> */}
+             <th scope="col">Date</th>
+             <th scope="col">Type</th>
             <th scope="col">Description</th>
-            <th scope="col">Income</th>
+           <th scope="col">Income</th>
             <th scope="col">Expenses</th>
-            <th scope="col">TotalAmount</th>
+                         <th scope="col">TotalAmount</th>
           </tr>
         </thead>
-        <tbody>
-          {this.state.financeRoute.map((financeRoute, index) => (
-            <tr key={index}>
-              <th scope="row">{index+1}</th>
+         <tbody>
+           {allDetails.map(finance => (
+            <tr>
+              <td>{finance.date}</td>
+              <td>{finance.type}</td>
+              <td>{finance.Description}</td>
+              <td>{finance.Income}</td>
+              <td>{finance.Expenses}</td>
+              <td>{finance.totalAmount}</td>
               <td>
-                <a href={`/financeRoute/${financeRoute._id}`} style={{textDecoration:'none'}}>
-                    {financeRoute.topic}
-                </a>
-              </td>
-              <td>{financeRoute.date}</td>
-              <td>{financeRoute.type}</td>
-              <td>{financeRoute.Description}</td>
-              <td>{financeRoute.Income}</td>
-              <td>{financeRoute.Expenses}</td>
-              <td>{financeRoute.totalAmount}</td>
-              <td>
-                <a className="btn btn-warning" href={`/editFinanceDetails/${financeRoute._id}`}>
-                  <i className="fas fa-edit"></i>&nbsp;edit
-                </a>
+               <Link to={`/update/${finance._id}`}>
+                <button type="button" className = "btn btn-warining">
+                  <i className = 'fas fa-edit'></i>&nbsp; Edit
+                </button>
+               </Link>
                 &nbsp;
-                <a className="btn btn-danger" href="#" onClick={()=>this.onDelete(financeRoute._id)}>
-                  <i className="far fa-trash-alt"></i>&nbsp;Delete
-                </a>
+              <button type="button" className='btn btn-danger' onClick={()=> handleDelete(finance._id)}>
+              <i className="far fa-trash-alt"></i>&nbsp;Delete
+              </button>
               </td>
             </tr>
           ))}
@@ -82,6 +95,11 @@ export default class ViewFinanceDetails extends Component {
        </table>
        <button className="btn btn-success"><a href="/createFinanceDetails" style={{textDecoration:'none', color:'white'}}>Create New Post</a></button>
       </div>
-    );
-  }
+
+)
+
+
+
 }
+
+export default ViewFianceDetails;
