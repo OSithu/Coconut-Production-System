16225e77891832178ebdc92fbb087bf9bfb1ef05
Heshara1127 <IT22207586@my.sliit.lk>
2024-04-11 20:50:58 +0530

    editCreate,Edit,ViewFiles_and_qcmpostsModelFile,qualityControlRoute

--- client/src/components/CreateQualityRecords.js
+++ client/src/components/CreateQualityRecords.js
@@ -1,155 +1,137 @@
-// import React, { Component } from "react";
-// import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-// export default class createQualityRecord extends Component {
-//   constructor(props) {
-//     super(props);
+const CreateQualityRecords = () => {
 
-//     this.state = {
-//         records:{}
-//     };
-//   }
+  const [recordId,setRecordId] =useState('');
+  const [productType,setProductType] =useState('');
+  const [qualityCheckedDate,setQualityCheckedDate] =useState('');
+  const [specialNotes,setSpecialNotes] =useState('');
+  const [testResult,setTestResult] =useState('');
 
-//    componentDidMount(){
-//     const id = this.props.match.params.id;
+  //implementing sendData function
+  const sendData = async (e) => {
+    e.preventDefault();
 
-//     axios.get('http://localhost:8000/records/$id').then((res) => {
-//         if(res.data.success){
-//              this.setState({
-//                 records:res.data.post
-//             });
-//             console.log(this.state.records);
-//         }
-//     });
-//    }
+    //  if (!RecordId || !ProductType || !QualityCheckedDate || !SpecialNotes || !TestResult) {
+    //    alert('Please fill in all fields');
+    //   return;
+    //  }
 
-//    render(){
+    //  // Validate QualityCheckedDate format (YYYY-MM-DD)
+    //  if (!/^\d{4}-\d{2}-\d{2}$/.test(QualityCheckedDate)) {
+    //   alert('Please enter a valid Quality Checked Date (YYYY-MM-DD)');
+    //   return;
+    // }
 
-//     const{Product,TestDate,TestResult} = this.state.records;
-
-//     return (
-//         <div style={{marginTop:'20px'}}>
-//             <h4>{Product}</h4>
-//             <hr/>
-
-//             <dl className ="row">
-//                 <dt className="col-sm-3">TestDate</dt>
-//                 <dd className="col-sm-9">{TestDate}</dd>
-//             </dl>
-//             <dl className ="row">
-//                 <dt className="col-sm-3">TestResult</dt>
-//                 <dd className="col-sm-9">{TestResult}</dd>
-//             </dl>
-//         </div>
-//     )
-//    }
-// }
-
-import React, { Component } from "react";
-import axios from "axios";
-
-export default class CreateQualityRecords extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          BatchId: "",
-          Product: "",
-          TestDate: "",
-          TestResult: "",
-        };
+    try{
+      let newQualityRecord = {
+        recordId:recordId,
+        productType:productType,
+        qualityCheckedDate:qualityCheckedDate,
+        specialNotes:specialNotes,
+        testResult:testResult,
       }
+      await axios.post('http://localhost:8000/qualityrecords/save',newQualityRecord)
+      .then((res) => {
+        alert(res.data.message);
+        console.log('status' +res.data.success);
+        console.log(res.data.message);
+      })
+      .catch((err) => {
+        if(err.response){
+          console.log(err.response.data.message);
+        }else{
+          console.log("Error occured while processing your axios post request" + err.message);
+        }
+      })
 
-      handleInputChange = (e) => {
-        const { name, value } = e.target;
+      setRecordId('');
+      setProductType();
+      setQualityCheckedDate();
+      setSpecialNotes();
+      setTestResult();
+
+    }
+    catch(err){
+      console.log('sendData function failed! ERROR' + err.message)
+    }
+  }  
+
+  return (
+    <div className="col-md-8 mt-4 mx-auto">
+      <h1 className="h3 mb-3 font-weight-normal">Add New Record</h1>
+      <form className="needs-validation" noValidate onSubmit={sendData}>
     
-        this.setState({
-          ...this.state,
-          [name]: value,
-        });
-      }
-
-      onSubmit = (e) => {
-        e.preventDefault();
+                    <div className="form-group" style={{marginBottom:'15px'}}>
+                            <label style={{marginBottom:'5px'}}>Record ID</label>
+                            <input
+                             type="text"
+                             className="form-control"
+                             name="recordId"
+                             placeholder="Enter RecordID "
+                             onChange={(e) => setRecordId(e.target.value)}
+                             value={recordId}
+                             required
+                            />
+                        </div>
     
-        const {BatchId,Product,TestDate,TestResult} = this.state;
+                        <div className="form-group" style={{marginBottom:'15px'}}>
+                            <label style={{marginBottom:'5px'}}>Product Type</label>
+                            <input type="text"
+                            className="form-control"
+                            name="productType"
+                            placeholder="Enter Product Type"
+                            onChange={(e) => setProductType(e.target.value)}
+                            value={productType}
+                            required
+                           />
+                        </div>
     
-        const data = {
-            BatchId:BatchId,
-            Product:Product,
-            TestDate:TestDate,
-            TestResult:TestResult,
-        };
+                        <div className="form-group" style={{marginBottom:'15px'}}>
+                            <label style={{marginBottom:'5px'}}>Quality Checked Date</label>
+                            <input type="Date"
+                            className="form-control"
+                            name="qualityCheckedDate"
+                            placeholder="Enter Quality Checked Date"
+                            onChange={(e) => setQualityCheckedDate(e.target.value)}
+                            value={qualityCheckedDate}
+                            required
+                           />
+                        </div>
+    
+                        <div className="form-group" style={{marginBottom:'15px'}}>
+                            <label style={{marginBottom:'5px'}}>Special Notes</label>
+                            <input type="text"
+                            className="form-control"
+                            name="specialNotes"
+                            placeholder="Enter Special Notes"
+                            onChange={(e) => setSpecialNotes(e.target.value)}
+                             value={specialNotes}
+                             required
+                            />
+                        </div>
+    
+                        <div className="form-group" style={{marginBottom:'15px'}}>
+                            <label style={{marginBottom:'5px'}}>Test Result</label>
+                            <input type="text"
+                            className="form-control"
+                            name="testResult"
+                            placeholder="Enter Test Result"
+                            onChange={(e) => setTestResult(e.target.value)}
+                            value={testResult}
+                            required
+                           />
+                        </div>
+    
+    
+                        <button className="btn btn-success" type="submit" style={{marginTop:'15px'}}>
+                            <i className="far fa check-square"></i>
+                            &nbsp: Save
+                        </button>
+      </form>
+    </div>
+    )
+  }
 
-        console.log(data);
-
-        axios.post("http://localhost:8000/qualityrecords/save", data).then((res) => {
-          if (res.data.success) {
-            this.setState({
-                BatchId: "",
-                Product: "",
-                TestDate: "",
-                TestResult: "",
-            });
-          }
-        });
-      }
-
-    render(){
-        return (
-            <div className="col-md-8 mt-4 mx-auto">
-                <h1 className="h3 mb-3 font-weight-normal">Add New Record</h1>
-                <form className="needs-validation" noValidate>
-
-                <div className="form-group" style={{marginBottom:'15px'}}>
-                        <label style={{marginBottom:'5px'}}>BatchId</label>
-                        <input type="text"
-                        className="form-control"
-                        name="BatchId"
-                        placeholder="Enter BatchId "
-                        value={this.state.BatchId}
-                        onChange={this.handleInputChange}/>
-                    </div>
-
-                    <div className="form-group" style={{marginBottom:'15px'}}>
-                        <label style={{marginBottom:'5px'}}>Product</label>
-                        <input type="text"
-                        className="form-control"
-                        name="Product"
-                        placeholder="Enter Product Name"
-                        value={this.state.Product}
-                        onChange={this.handleInputChange}/>
-                    </div>
-
-                    <div className="form-group" style={{marginBottom:'15px'}}>
-                        <label style={{marginBottom:'5px'}}>TestDate</label>
-                        <input type="date"
-                        className="form-control"
-                        name="TestDate"
-                        placeholder="Enter Test Date"
-                        value={this.state.TestDate}
-                        onChange={this.handleInputChange}/>
-                    </div>
-
-                    <div className="form-group" style={{marginBottom:'15px'}}>
-                        <label style={{marginBottom:'5px'}}>TestResult</label>
-                        <input type="text"
-                        className="form-control"
-                        name="TestResult"
-                        placeholder="Enter Test Result"
-                        value={this.state.TestResult}
-                        onChange={this.handleInputChange}/>
-                    </div>
-
-                    <button className="btn btn-success" type="submit" style={{marginTop:'15px'}} onClick={this.onSubmit}>
-                        <i className="far fa check-square"></i>
-                        &nbsp: Save
-                    </button>
-
-                    
-
-                </form>
-            </div>
-        )
-    }
-}
-
+export default CreateQualityRecords;
