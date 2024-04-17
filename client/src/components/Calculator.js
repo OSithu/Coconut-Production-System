1588c4ef1248f3b560905d1cdb53cb6fdb088938
Nimsi Dharmaratne <nimsidinara@gmail.com>
2024-04-17 21:15:51 +0530

    update calculator logic and styles

--- client/src/components/Calculator.js
+++ client/src/components/Calculator.js
@@ -15,7 +15,7 @@ function Calculator() {
     let muriateAmount = 0;
     let dolomiteAmount = 0;
 
-    if (age >= 48) {
+    if (age > 48) {
       if (zone === "wet") {
         ureaAmount = 3300.0 * (8 / 33);
         phosphateAmount = 3300.0 * (9 / 33);
@@ -25,7 +25,7 @@ function Calculator() {
         phosphateAmount = 2800.0 * (4 / 28);
         muriateAmount = 2800.0 * (16 / 28);
       }
-    } else if (age >= 6 && age <= 48) {
+    } else if (age=== 6) {
       if (zone === "wet") {
         ureaAmount = 800.0 * (2 / 8.5);
         phosphateAmount = 800.0 * (4.5 / 8.5);
@@ -35,7 +35,47 @@ function Calculator() {
         phosphateAmount = 540.0 * (1 / 3);
         muriateAmount = 540.0 * (1 / 3);
       }
-    } else {
+    } else if (age=== 12||age===18) {
+        if (zone === "wet") {
+          ureaAmount = 1000.0 * (2 / 8.5);
+          phosphateAmount = 1000.0 * (4.5 / 8.5);
+          muriateAmount = 1000.0 * (2 / 8.5);
+        } else {
+          ureaAmount = 670.0 * (1 / 3);
+          phosphateAmount = 670.0 * (1 / 3);
+          muriateAmount = 670.0 * (1 / 3);
+        }
+    } else if (age=== 24||age===30) {
+        if (zone === "wet") {
+          ureaAmount = 1300.0 * (2 / 8.5);
+          phosphateAmount = 1300.0 * (4.5 / 8.5);
+          muriateAmount = 1300.0 * (2 / 8.5);
+        } else {
+          ureaAmount = 905.0 * (1 / 3);
+          phosphateAmount = 905.0 * (1 / 3);
+          muriateAmount = 905.0 * (1 / 3);
+        }
+    } else if (age===36||age===42) {
+        if (zone === "wet") {
+          ureaAmount = 1600.0 * (2 / 8.5);
+          phosphateAmount = 1600.0 * (4.5 / 8.5);
+          muriateAmount = 1600.0 * (2 / 8.5);
+        } else {
+          ureaAmount = 1110.0 * (1 / 3);
+          phosphateAmount = 1110.0 * (1 / 3);
+          muriateAmount = 1110.0 * (1 / 3);
+        }
+    } else if (age=== 48) {
+        if (zone === "wet") {
+          ureaAmount = 2000.0 * (2 / 8.5);
+          phosphateAmount = 2000.0 * (4.5 / 8.5);
+          muriateAmount = 2000.0 * (2 / 8.5);
+        } else {
+          ureaAmount = 1340.0 * (1 / 3);
+          phosphateAmount = 1340.0 * (1 / 3);
+          muriateAmount = 1340.0 * (1 / 3);
+        }
+    } else if(age===0) {
       if (zone === "wet") {
         ureaAmount = 1250.0 * (1 / 5);
         phosphateAmount = 1250.0 * (3 / 5);
@@ -47,9 +87,10 @@ function Calculator() {
       }
     }
 
+
     if (age === 0) {
       dolomiteAmount = 1000;
-    } else if (age === 6) {
+    } else if (6<=age<=48) {
       dolomiteAmount = 500;
     } else {
       dolomiteAmount = 1000;
@@ -99,19 +140,20 @@ function Calculator() {
           <option value="dry">Dry Zone</option>
           <option value="wet">Wet Zone</option>
         </select>
-        <label htmlFor="age">Tree's Age (months/years):</label>
+        <label htmlFor="age">Tree's Age (months/years):<br></br><label className="age-message">**if newly planted,select 0 months</label></label>
         <select
           id="age"
           value={age}
           onChange={handleAgeChange}
         >
+          <option value="0">0 months</option>
           <option value="6">6 months</option>
           <option value="12">12 months(1year)</option>
-          <option value="18">18 months(1year-6months)</option>
+          <option value="18">18 months(1year&6months)</option>
           <option value="24">24 months(2year)</option>
-          <option value="30">30 months(2year-6months)</option>
+          <option value="30">30 months(2year&6months)</option>
           <option value="36">36 months(3year)</option>
-          <option value="42">42 months(3year-6months)</option>
+          <option value="42">42 months(3year&6months)</option>
           <option value="48">48 months(4year)</option>
           <option value="50">more than 4years</option>
         </select>
@@ -123,7 +165,7 @@ function Calculator() {
         <div>
       <p>Urea: <span>{urea}g</span></p>
       <p>EppawalaRock Phosphate: <span>{phosphate}g</span></p>
-      <p>MuriateOf Potasium: <span>{muriate}g</span></p>
+      <p>Muriate Of Potasium: <span>{muriate}g</span></p>
       <p>Dolomite: <span>{dolomite}g</span></p>
     </div>
       </div>
