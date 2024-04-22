014ffd448cb488a61d3b921ccc7aab829bba54fa
Osh <107048011+it22368348@users.noreply.github.com>
2024-04-23 01:43:57 +0530

    add boostrap design to edit form

--- client/src/components/editSpread.js
+++ client/src/components/editSpread.js
@@ -9,16 +9,14 @@ const EditSpread = () => {
   const [spreadLevel, setspreadLevel] = useState("");
   const [specialNote, setspecialNote] = useState("");
 
-    // State variables for form errors
-    const [errors, setErrors] = useState({
-      treeID: '',
-      identifyDate: '',
-      disease: '',
-      spreadLevel: '',
-      specialNote: '',
- 
+  // State variables for form errors
+  const [errors, setErrors] = useState({
+    treeID: "",
+    identifyDate: "",
+    disease: "",
+    spreadLevel: "",
+    specialNote: "",
   });
-
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -136,113 +134,135 @@ const EditSpread = () => {
   };
 
   return (
-    <div>
-      <h1>Edit Record</h1>
-      <form className="needs-validation" noValidate onSubmit={updateData}>
-        <div className="form-group" style={{ marginBottom: "15px" }}>
-          <label style={{ marginBottom: "5px" }}>Tree ID</label>
-          <input
-            type="text"
-            className={`form-control ${errors.treeID ? "is-invalid" : ""}`}
-            name="treeID"
-            placeholder="Enter Tree ID"
-            onChange={(e) => settreeID(e.target.value)}
-            value={treeID}
-          />
-          {errors.treeID && (
-            <div className="invalid-feedback">{errors.treeID}</div>
-          )}
-        </div>
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center">Edit Spread Records</h1>
+              <form
+                className="needs-validation"
+                noValidate
+                onSubmit={updateData}
+              >
+                <div className="form-group" style={{ marginBottom: "15px" }}>
+                  <label style={{ marginBottom: "5px" }}>Tree ID</label>
+                  <input
+                    type="text"
+                    className={`form-control ${
+                      errors.treeID ? "is-invalid" : ""
+                    }`}
+                    name="treeID"
+                    placeholder="Enter Tree ID"
+                    onChange={(e) => settreeID(e.target.value)}
+                    value={treeID}
+                  />
+                  {errors.treeID && (
+                    <div className="invalid-feedback">{errors.treeID}</div>
+                  )}
+                </div>
 
-        <div className="form-group" style={{ marginBottom: "15px" }}>
-          <label style={{ marginBottom: "5px" }}>Identify Date</label>
-          <input
-            type="date"
-            className={`form-control ${
-              errors.identifyDate ? "is-invalid" : ""
-            }`}
-            name="identifyDate"
-            placeholder="Enter Identify Date"
-            onChange={(e) => setidentifyDate(e.target.value)}
-            value={identifyDate}
-          />
-          {errors.identifyDate && (
-            <div className="invalid-feedback">{errors.identifyDate}</div>
-          )}
-        </div>
+                <div className="form-group" style={{ marginBottom: "15px" }}>
+                  <label style={{ marginBottom: "5px" }}>Identify Date</label>
+                  <input
+                    type="date"
+                    className={`form-control ${
+                      errors.identifyDate ? "is-invalid" : ""
+                    }`}
+                    name="identifyDate"
+                    placeholder="Enter Identify Date"
+                    onChange={(e) => setidentifyDate(e.target.value)}
+                    value={identifyDate}
+                  />
+                  {errors.identifyDate && (
+                    <div className="invalid-feedback">
+                      {errors.identifyDate}
+                    </div>
+                  )}
+                </div>
 
-        <div className="form-group" style={{ marginBottom: "15px" }}>
-          <label style={{ marginBottom: "5px" }}>Disease</label>
-          <input
-            type="text"
-            className={`form-control ${errors.disease ? "is-invalid" : ""}`}
-            name="disease"
-            placeholder="Enter Disease"
-            onChange={(e) => setdisease(e.target.value)}
-            value={disease}
-          />
-          {errors.disease && (
-            <div className="invalid-feedback">{errors.disease}</div>
-          )}
-        </div>
+                <div className="form-group" style={{ marginBottom: "15px" }}>
+                  <label style={{ marginBottom: "5px" }}>Disease</label>
+                  <input
+                    type="text"
+                    className={`form-control ${
+                      errors.disease ? "is-invalid" : ""
+                    }`}
+                    name="disease"
+                    placeholder="Enter Disease"
+                    onChange={(e) => setdisease(e.target.value)}
+                    value={disease}
+                  />
+                  {errors.disease && (
+                    <div className="invalid-feedback">{errors.disease}</div>
+                  )}
+                </div>
 
-        <div className="form-group" style={{ marginBottom: "15px" }}>
-          <label style={{ marginBottom: "5px" }}>Spread Level</label>
-          <select
-            className={`form-control ${errors.spreadLevel ? "is-invalid" : ""}`}
-            name="spreadLevel"
-            onChange={(e) => setspreadLevel(e.target.value)}
-            value={spreadLevel}
-          >
-            <option value="">Select a Spread Level</option>
-            <option
-              value="High"
-              style={{ fontWeight: "bold", backgroundColor: "red" }}
-            >
-              High
-            </option>
-            <option
-              value="Medium"
-              style={{ fontWeight: "bold", backgroundColor: "yellow" }}
-            >
-              Medium
-            </option>
-            <option
-              value="Low"
-              style={{ fontWeight: "bold", backgroundColor: "green" }}
-            >
-              Low
-            </option>
-          </select>
-          {errors.spreadLevel && (
-            <div className="invalid-feedback">{errors.spreadLevel}</div>
-          )}
-        </div>
+                <div className="form-group" style={{ marginBottom: "15px" }}>
+                  <label style={{ marginBottom: "5px" }}>Spread Level</label>
+                  <select
+                    className={`form-control ${
+                      errors.spreadLevel ? "is-invalid" : ""
+                    }`}
+                    name="spreadLevel"
+                    onChange={(e) => setspreadLevel(e.target.value)}
+                    value={spreadLevel}
+                  >
+                    <option value="">Select a Spread Level</option>
+                    <option
+                      value="High"
+                      style={{ fontWeight: "bold", backgroundColor: "red" }}
+                    >
+                      High
+                    </option>
+                    <option
+                      value="Medium"
+                      style={{ fontWeight: "bold", backgroundColor: "yellow" }}
+                    >
+                      Medium
+                    </option>
+                    <option
+                      value="Low"
+                      style={{ fontWeight: "bold", backgroundColor: "green" }}
+                    >
+                      Low
+                    </option>
+                  </select>
+                  {errors.spreadLevel && (
+                    <div className="invalid-feedback">{errors.spreadLevel}</div>
+                  )}
+                </div>
 
-        <div className="form-group" style={{ marginBottom: "15px" }}>
-          <label style={{ marginBottom: "5px" }}>Special Note</label>
-          <input
-            type="text"
-            className={`form-control ${errors.specialNote ? "is-invalid" : ""}`}
-            name="specialNote"
-            placeholder="Enter Special Note"
-            onChange={(e) => setspecialNote(e.target.value)}
-            value={specialNote}
-          />
-          {errors.specialNote && (
-            <div className="invalid-feedback">{errors.specialNote}</div>
-          )}
-        </div>
+                <div className="form-group" style={{ marginBottom: "15px" }}>
+                  <label style={{ marginBottom: "5px" }}>Special Note</label>
+                  <input
+                    type="text"
+                    className={`form-control ${
+                      errors.specialNote ? "is-invalid" : ""
+                    }`}
+                    name="specialNote"
+                    placeholder="Enter Special Note"
+                    onChange={(e) => setspecialNote(e.target.value)}
+                    value={specialNote}
+                  />
+                  {errors.specialNote && (
+                    <div className="invalid-feedback">{errors.specialNote}</div>
+                  )}
+                </div>
 
-        <button
-          className="btn btn-success"
-          type="submit"
-          style={{ marginTop: "15px" }}
-        >
-          <i className="far fa-check-square"></i>
-          &nbsp; Save
-        </button>
-      </form>
+                <button
+                  className="btn btn-success"
+                  type="submit"
+                  style={{ marginTop: "15px" }}
+                >
+                  <i className="far fa-check-square"></i>
+                  &nbsp; Save
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
