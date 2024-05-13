94ed6b9d23fb91bd8548e3f97f31606a7c6b5b17
Dishani Kulasekara <149486947+dish1307@users.noreply.github.com>
2024-05-13 12:01:56 +0530

    edit css files

--- client/src/components/AddBudgetDetails.js
+++ client/src/components/AddBudgetDetails.js
@@ -50,6 +50,7 @@ const AddBudgetDetails = () => {
     return (
         <div className="col-md-8 mt-4 mx-auto">
             <h1 className="h3 mb-3 font-weight-normal">Add Budget Details</h1>
+            <div id ="plantForm">
             <form className="needs-validation" noValidate>
                 <div className="form-group" style={{ marginBottom: '15px' }}>
                     <label style={{ marginBottom: '5px' }}>Month</label>
@@ -120,6 +121,7 @@ const AddBudgetDetails = () => {
                     &nbsp; Save
                 </button>
             </form>
+            </div>
         </div>
     );
 };
